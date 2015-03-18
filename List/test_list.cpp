d9070eb2576c75b82b9625dd7d700402454e59ec
RaulButuc <raulbutuc@gmail.com>
2015-03-18 21:52:40 +0000

    Heavily improved the testing

--- List/test_list.cpp
+++ List/test_list.cpp
@@ -1,5 +1,5 @@
 /**
- *  Test.cpp
+ *  test_list.cpp
  *
  *  @author Raul Butuc.
  *  @version 1.0.1 18/03/2015
@@ -14,41 +14,64 @@ using std::find;
 using namespace my_library;
 
 int main(int argc, char* argv[]) {
-  list<char> my_list;
+  // Declare a new singly linked list with the first 5 elements initialised to 'y'.
+  list<char> my_list(3, 'Y');
 
-  if (my_list.empty()) {
-    cout << "List is empty" << "\n";
-  }
+  // Check whether the list is or is not empty.
+  cout << " o Checking the state of my_list: ";
+  cout << "The list is" << (my_list.empty() ? "" : " not") << " empty" << "\n";
 
+  // Add some characters to the list.
   for (char i = 'a'; i < 'z'; i += 2) {
     my_list.push_back(i);
   }
   
+  // Get an iterator for the first element.
   list<char>::iterator it = my_list.begin();
-
+  
+  // Loop through the list and prove its mutability.
+  cout << " o Printing the elements of my_list: ";
   for (; it != my_list.end(); ++it) {
+    *it += 1;
     cout << *it << " ";
   }
   cout << "\n";
 
-  char first_value = 'w';
-  char second_value = 'S';
+  // Take two values, one that exists in the list and one that doesn't.
+  char first_value = 'x';
+  char second_value = 'H';
 
+  // Get an iterator to first_value, if found; then print out a message.
   it = find(my_list.begin(), my_list.end(), first_value);
-  if (it != my_list.end()) {
-    cout << "Element " << first_value << " found!" << "\n";
-  }
-  else {
-    cout << "Element " << first_value << " not found!" << "\n";
-  }
-
+  cout << " o Testing 'find' in my_list: ";
+  cout << "Element " << first_value << (it != my_list.end() ? "" : " not") << " found!" << "\n";
+  
+  // Get an iterator to second_value, if found; then print out a message.
   it = find(my_list.begin(), my_list.end(), second_value);
-  if (it != my_list.end()) {
-    cout << "Element " << second_value << " found!" << "\n";
+  cout << " o Testing 'find' in my_list: ";
+  cout << "Element " << second_value << (it != my_list.end() ? "" : " not") << " found!" << "\n";
+
+  // Create a copy of the first list, by using a constructor which takes iterators as arguments.
+  list<char> copy_my_list(my_list.begin(), my_list.end());
+
+  // Get an iterator for the first element of the copy of the list and print the list out.
+  list<char>::iterator copy_it = copy_my_list.begin();
+  cout << " o Printing the elements of copy_my_list: ";
+  for (; copy_it != copy_my_list.end(); ++copy_it) {
+    cout << *copy_it << " ";
   }
-  else {
-    cout << "Element " << second_value << " not found!" << "\n";
+  cout << "\n";
+
+  // Create a constant copy of the first list, by using a constructor which takes a list as an arguement.
+  const list<char> const_copy_my_list(my_list);
+
+  // Get a const_iterator for the first element of the other copy of the list and print the list out.
+  list<char>::const_iterator const_copy_it = const_copy_my_list.begin();
+  cout << " o Printing the elements of const_copy_my_list: ";
+  for (; const_copy_it != const_copy_my_list.end(); ++const_copy_it) {
+    cout << *const_copy_it << " ";
   }
+  cout << "\n";
 
   system("pause");
   return 0;
