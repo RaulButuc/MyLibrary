b083fd75f61e211943aa0cd39968176dad40518f
RaulButuc <raulbutuc@gmail.com>
2015-03-19 18:29:16 +0000

    Added more constructors and functions

--- List/list.h
+++ List/list.h
@@ -2,11 +2,12 @@
  *  list.h
  *
  *  @author Raul Butuc.
- *  @version 1.0.2 18/03/2015
+ *  @version 1.0.3 19/03/2015
  */
 
 #pragma once
 
+#include <algorithm>
 #include "list_node.h"
 #include "list_iterator.h"
 #include "list_const_iterator.h"
@@ -24,11 +25,18 @@ namespace my_library {
       explicit list(size_t, const _Tp&);
       list(iterator, iterator);
       list(const_iterator, const_iterator);
-      list(const list&);
+      list(const list<_Tp>&);
+      list(list<_Tp>&&);
       ~list();
 
+      void assign(size_t, const _Tp&);
+      void assign(iterator, iterator);
+      void assign(const_iterator, const_iterator);
+      void clear();
       bool empty() const;
-      void push_back(const _Tp&);
+      void push(const _Tp&);
+      void pop();
+      size_t size() const;
 
       iterator begin();
       iterator end();
@@ -38,38 +46,45 @@ namespace my_library {
     private:
       list_node<_Tp>* m_pHead;
       list_node<_Tp>* m_pTail;
+      size_t m_Size;
   };
   
   template <class _Tp>
-  list<_Tp>::list() : m_pHead(NULL), m_pTail(NULL) {}
+  list<_Tp>::list() : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {}
 
   template <class _Tp>
-  list<_Tp>::list(size_t nr, const _Tp& value) {
+  list<_Tp>::list(size_t nr, const _Tp& value) : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {
     for (size_t index = 0; index < nr; ++index) {
-      push_back(value);
+      push(value);
     }
   }
 
   template <class _Tp>
-  list<_Tp>::list(iterator first, iterator last) {
+  list<_Tp>::list(iterator first, iterator last) : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {
     for (; first != last; ++first) {
-      push_back(*first);
+      push(*first);
     }
   }
 
   template <class _Tp>
-  list<_Tp>::list(const_iterator first, const_iterator last) {
+  list<_Tp>::list(const_iterator first, const_iterator last) : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {
     for (; first != last; ++first) {
-      push_back(*first);
+      push(*first);
     }
   }
 
   template <class _Tp>
-  list<_Tp>::list(const list& _list) {
+  list<_Tp>::list(const list<_Tp>& _list) : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {
     const_iterator it = _list.begin();
     for (; it != _list.end(); ++it) {
-      push_back(*it);
+      push(*it);
     }
+  }
+
+  template <class _Tp>
+  list<_Tp>::list(list<_Tp>&& _list) : m_pHead(nullptr), m_pTail(nullptr), m_Size(0) {
+    std::move(_list.begin(), _list.end(), *this);
+    _list.~list();
   }
 
   template <class _Tp>
@@ -78,20 +93,70 @@ namespace my_library {
   }
 
   template <class _Tp>
+  void list<_Tp>::assign(size_t nr, const _Tp& value) {
+    clear();
+    for (size_t index = 0; index < nr; ++index) {
+      push(value);
+    }
+  }
+
+  template <class _Tp>
+  void list<_Tp>::assign(iterator first, iterator last) {
+    clear();
+    for (; first != last; ++first) { 
+      push(*first);
+    }
+  }
+
+  template <class _Tp>
+  void list<_Tp>::assign(const_iterator first, const_iterator last) {
+    clear();
+    for (; first != last; ++first) {
+      push(*first);
+    }
+  }
+
+  template <class _Tp>
+  void list<_Tp>::clear() {
+    while (m_pHead != nullptr) { pop(); }
+  }
+
+  template <class _Tp>
   bool list<_Tp>::empty() const {
-    return m_pHead == NULL;
+    return m_pHead == nullptr;
   }
   
   template <class _Tp>
-  void list<_Tp>::push_back(const _Tp& node) {
-    list_node<_Tp>* _node = new list_node<_Tp>(node, NULL);
-    if (m_pHead == NULL) {
-      m_pHead = _node;
-    }
-    else {
-      m_pTail->m_pNext = _node;
-    }
+  void list<_Tp>::push(const _Tp& node) {
+    list_node<_Tp>* _node = new list_node<_Tp>(node, nullptr);
+
+    if (m_pHead == nullptr) { m_pHead = _node; }
+    else { m_pTail->m_pNext = _node; }
+
     m_pTail = _node;
+    m_Size++;
+  }
+
+  template <class _Tp>
+  void list<_Tp>::pop() {
+    iterator current = begin();
+    iterator prev = nullptr;
+    iterator last = current;
+    
+    while (current != end()) {
+      prev = last;
+      last = current;
+      ++current;
+    }
+
+    delete last.m_pNode;
+    prev.m_pNode->m_pNext = nullptr;
+    m_Size--;
+  }
+
+  template <class _Tp>
+  size_t list<_Tp>::size() const {
+    return m_Size;
   }
 
   template <class _Tp>
@@ -101,7 +166,7 @@ namespace my_library {
 
   template <class _Tp>
   typename list<_Tp>::iterator list<_Tp>::end() {
-    return list_iterator<_Tp>(NULL);
+    return list_iterator<_Tp>(nullptr);
   }
 
   template <class _Tp>
@@ -111,7 +176,7 @@ namespace my_library {
 
   template <class _Tp>
   typename list<_Tp>::const_iterator list<_Tp>::end() const {
-    return list_const_iterator<_Tp>(NULL);
+    return list_const_iterator<_Tp>(nullptr);
   }
 
 }
