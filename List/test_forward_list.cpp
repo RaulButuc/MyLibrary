f8211f07af6b6e16045eaf98256b6ba176776818
RaulButuc <raulbutuc@gmail.com>
2015-03-21 18:04:48 +0000

    Name change and new tests added

--- List/test_forward_list.cpp
+++ List/test_forward_list.cpp
@@ -1,14 +1,14 @@
 /**
- *  test_list.cpp
+ *  test_forward_list.cpp
  *
  *  @author Raul Butuc.
- *  @version 1.0.3 19/03/2015
+ *  @version 1.1.0 21/03/2015
  */
 
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include "list.h"
+#include "forward_list.h"
 
 using std::cout;
 using std::find;
@@ -16,8 +16,8 @@ using std::string;
 using namespace my_library;
 
 int main(int argc, char* argv[]) {
-  // Create a new singly linked list with the first 3 elements initialised to 'Y'.
-  list<char> my_list(3, 'Y');
+  // Declare a new singly linked list with the first 5 elements initialised to 'Y'.
+  forward_list<char> my_list(3, 'Y');
 
   // Check whether the list is or is not empty.
   cout << " o Checking the state of my_list: ";
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
   }
   
   // Get an iterator for the first element.
-  list<char>::iterator it = my_list.begin();
+  forward_list<char>::iterator it = my_list.begin();
   
   // Loop through the list and prove its mutability.
   cout << " o Printing the elements of my_list: ";
@@ -54,10 +54,10 @@ int main(int argc, char* argv[]) {
   cout << "Element " << second_value << (it != my_list.end() ? "" : " not") << " found!" << "\n";
 
   // Create a copy of the first list, by using a constructor which takes iterators as arguments.
-  list<char> copy_my_list(my_list.begin(), my_list.end());
+  forward_list<char> copy_my_list(my_list.begin(), my_list.end());
 
   // Get an iterator for the first element of the copy of the list and print the list out.
-  list<char>::iterator copy_it = copy_my_list.begin();
+  forward_list<char>::iterator copy_it = copy_my_list.begin();
   cout << " o Printing the elements of copy_my_list: ";
   for (; copy_it != copy_my_list.end(); ++copy_it) {
     cout << *copy_it << " ";
@@ -65,10 +65,10 @@ int main(int argc, char* argv[]) {
   cout << "(" << copy_my_list.size() << " elements)" << "\n";
 
   // Create a constant copy of the first list, by using a constructor which takes a list as an arguement.
-  const list<char> const_copy_my_list(my_list);
+  const forward_list<char> const_copy_my_list(my_list);
 
   // Get a const_iterator for the first element of the other copy of the list and print the list out.
-  list<char>::const_iterator const_copy_it = const_copy_my_list.begin();
+  forward_list<char>::const_iterator const_copy_it = const_copy_my_list.begin();
   cout << " o Printing the elements of const_copy_my_list: ";
   for (; const_copy_it != const_copy_my_list.end(); ++const_copy_it) {
     cout << *const_copy_it << " ";
@@ -77,10 +77,10 @@ int main(int argc, char* argv[]) {
 
   // Create a new list using the move constructor, moving the temporary list into the new_list and then deallocating
   // the temporary memory used.
-  list<string> new_list(list<string>(7, "Abc"));
+  forward_list<string> new_list(forward_list<string>(7, "Abc"));
 
   // Get an iterator for the first element of the new list and print the list out.
-  list<string>::iterator new_list_it = new_list.begin();
+  forward_list<string>::iterator new_list_it = new_list.begin();
   cout << " o Printing the elements of new_list: ";
   for (; new_list_it != new_list.end(); ++new_list_it) {
     cout << *new_list_it << " ";
@@ -98,6 +98,10 @@ int main(int argc, char* argv[]) {
     cout << *new_list_it << " ";
   }
   cout << "(" << new_list.size() << " elements)" << "\n";
+
+  // Get the last element of the first list and print it out.
+  cout << " o Print the last element of my_list: ";
+  cout << my_list.back() << "\n";
 
   system("pause");
   return 0;
