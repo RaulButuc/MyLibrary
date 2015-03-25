fbf928a0bc40889ecd32020f94ca3c1244219640
RaulButuc <raulbutuc@gmail.com>
2015-03-25 18:58:03 +0200

    Re-built tests

--- Forward List/test_forward_list.cpp
+++ Forward List/test_forward_list.cpp
@@ -2,7 +2,7 @@
  *  test_forward_list.cpp
  *
  *  @author Raul Butuc.
- *  @version 1.1.2 24/03/2015
+ *  @version 1.2.0 25/03/2015
  */
 
 #include <iostream>
@@ -16,118 +16,192 @@ using std::find;
 using std::string;
 using my_library::forward_list;
 
+void test_forward_list_one();
+void test_forward_list_two();
+void test_forward_list_three();
+void test_forward_list_four();
+
 forward_list<char> addOne(const forward_list<char>&);
 
 int main(int argc, char* argv[]) {
-  // Declare a new singly linked list with the first 5 elements initialised to 'Y'.
+  test_forward_list_one();
+  test_forward_list_two();
+  test_forward_list_three();
+  test_forward_list_four();
+
+  system("pause");
+  return 0;
+}
+
+void test_forward_list_one() {
+  cout << "\n" << "----------------- TEST 1 -----------------" << "\n";
+
+  cout << " - Create a new list (3, 'Y')" << "\n";
   forward_list<char> my_list(3, 'Y');
 
-  // Check whether the list is or is not empty.
+  forward_list<char>::iterator it = my_list.begin();
+  cout << " o Printing the elements of my_list: ";
+  for (; it != my_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
   cout << " o Checking the state of my_list: ";
   cout << "The list is" << (my_list.empty() ? "" : " not") << " empty" << "\n";
 
-  // Add some characters to the list.
+  cout << " - Push in front the characters from 'a' to 'y' in steps of 2, then reverse the list" << "\n";
   for (char i = 'a'; i < 'z'; i += 2) {
     my_list.push_front(i);
   }
-
-  // Reverse the list.
   my_list.reverse();
-  
-  // Get an iterator for the first element.
-  forward_list<char>::iterator it = my_list.begin();
-  
-  // Loop through the list and prove its mutability.
+
+  it = my_list.begin();
   cout << " o Printing the elements of my_list: ";
   for (; it != my_list.end(); ++it) {
-    *it += 1;
     cout << *it << " ";
   }
   cout << "\n";
 
-  // Take two values, one that exists in the list and one that doesn't.
-  char first_value = 'x';
+  char first_value = 'y';
   char second_value = 'H';
 
-  // Get an iterator to first_value, if found; then print out a message.
   it = find(my_list.begin(), my_list.end(), first_value);
-  cout << " o Testing 'find' in my_list: ";
+  cout << " o Testing std::find in my_list: ";
   cout << "Element " << first_value << (it != my_list.end() ? "" : " not") << " found!" << "\n";
-  
-  // Get an iterator to second_value, if found; then print out a message.
+
   it = find(my_list.begin(), my_list.end(), second_value);
-  cout << " o Testing 'find' in my_list: ";
+  cout << " o Testing std::find in my_list: ";
   cout << "Element " << second_value << (it != my_list.end() ? "" : " not") << " found!" << "\n";
+}
 
-  // Create a copy of the first list, by using a constructor which takes iterators as arguments.
-  forward_list<char> copy_my_list(my_list.begin(), my_list.end());
+void test_forward_list_two() {
+  cout << "\n" << "----------------- TEST 2 -----------------" << "\n";
 
-  // Get an iterator for the first element of the copy of the list and print the list out.
-  forward_list<char>::iterator copy_it = copy_my_list.begin();
-  cout << " o Printing the elements of copy_my_list: ";
-  for (; copy_it != copy_my_list.end(); ++copy_it) {
-    cout << *copy_it << " ";
-  }
-  cout << "\n";
+  cout << " - Create a new empty list" << "\n";
+  forward_list<char> my_list;
 
-  // Create a constant copy of the first list, by using a constructor which takes a list as an arguement.
-  const forward_list<char> const_copy_my_list(my_list);
+  cout << " o Checking the state of my_list: ";
+  cout << "The list is" << (my_list.empty() ? "" : " not") << " empty" << "\n";
 
-  // Get a const_iterator for the first element of the other copy of the list and print the list out.
-  forward_list<char>::const_iterator const_copy_it = const_copy_my_list.begin();
-  cout << " o Printing the elements of const_copy_my_list: ";
-  for (; const_copy_it != const_copy_my_list.end(); ++const_copy_it) {
-    cout << *const_copy_it << " ";
-  }
-  cout << "\n";
+  cout << " - Assign the new list (5, '@')" << "\n";
+  my_list.assign(5, '@');
+ 
+  forward_list<char>::iterator it = my_list.begin();
+  forward_list<char>::const_iterator cIt = my_list.cbegin();
 
-  // Create a new list using the move constructor, moving the temporary list into the new_list and then deallocating
-  // the temporary memory used.
-  forward_list<string> new_list(forward_list<string>(7, "Abc"));
-
-  // Get an iterator for the first element of the new list and print the list out.
-  forward_list<string>::iterator new_list_it = new_list.begin();
-  cout << " o Printing the elements of new_list: ";
-  for (; new_list_it != new_list.end(); ++new_list_it) {
-    cout << *new_list_it << " ";
-  }
-  cout << "\n";
-
-  // Pop the first two elements.
-  new_list.pop_front();
-  new_list.pop_front();
-
-  // Get an iterator for the first element of the new list and print the list out.
-  new_list_it = new_list.begin();
-  cout << " o Printing the elements of new_list (after popping the first two): ";
-  for (; new_list_it != new_list.end(); ++new_list_it) {
-    cout << *new_list_it << " ";
-  }
-  cout << "\n";
-
-  // Pop the first three elements in my_list.
-  my_list.pop_front();
-  my_list.pop_front();
-  my_list.pop_front();
-
-  // Get an iterator for the first element.
-  it = my_list.begin();
-
-  // Loop through the list and prove its mutability.
-  cout << " o Printing the elements of my_list (after popping the first three): ";
+  cout << " o Printing the elements of my_list (after assignment): ";
   for (; it != my_list.end(); ++it) {
     cout << *it << " ";
   }
   cout << "\n";
 
-  // Get the first element of the first list and print it out.
-  cout << " o Print the first element of my_list: ";
-  cout << my_list.front() << "\n";
+  cout << " - Set the constant iterator to point 3 steps ahead from the beginning." << "\n";
+  cIt += 3;
   
-  // Create a new list.
+  cout << " - Insert after the current constant iterator position the 'w' char (by moving)" << "\n";
+  my_list.insert_after(cIt, 'w');
+
+  it = my_list.begin();
+  cout << " o Printing the elements of my_list: ";
+  for (; it != my_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " - Increment the constant iterator 1 step, then insert after it 3 times 'z' (++cIt, 3, 'z')" << "\n";
+  my_list.insert_after(++cIt, 3, 'z');
+
+  it = my_list.begin();
+  cout << " o Printing the elements of my_list: ";
+  for (; it != my_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " - Create a copy of my_list (copy_my_list) by passing the begin and end iterators" << "\n";
+  forward_list<char> copy_my_list(my_list.begin(), my_list.end());
+
+  forward_list<char>::iterator copyIt = copy_my_list.begin();
+  cout << " o Printing the elements of copy_my_list: ";
+  for (; copyIt != copy_my_list.end(); ++copyIt) {
+    cout << *copyIt << " ";
+  }
+  cout << "\n";
+}
+
+void test_forward_list_three() {
+  cout << "\n" << "----------------- TEST 3 -----------------" << "\n";
+
+  cout << " - Create a new list from a temporary list new_list(forward_list<string>(7, \"Abc\"));" << "\n";
+  forward_list<string> new_list(forward_list<string>(7, "Abc"));
+
+  forward_list<string>::iterator it = new_list.begin();
+  cout << " o Printing the elements of new_list: ";
+  for (; it != new_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " - Push front the string \"World!\"" << "\n";
+  new_list.push_front("World!");
+
+  it = new_list.begin();
+  cout << " o Printing the elements of new_list: ";
+  for (; it != new_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " - Push front the string \"Hello\"" << "\n";
+  new_list.push_front("Hello");
+
+  it = new_list.begin();
+  cout << " o Printing the elements of new_list: ";
+  for (; it != new_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " o Print the first element of new_list (using the front function): ";
+  cout << new_list.front() << "\n";
+  
+  cout << " - Pop the first five elements" << "\n";
+  new_list.pop_front();
+  new_list.pop_front();
+  new_list.pop_front();
+  new_list.pop_front();
+  new_list.pop_front();
+
+  it = new_list.begin();
+  cout << " o Printing the elements of new_list (after popping the first five): ";
+  for (; it != new_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+}
+
+void test_forward_list_four() {
+  cout << "\n" << "----------------- TEST 4 -----------------" << "\n";
+
+  cout << " - Create a new list (empty)" << "\n";
+  forward_list<char> my_list;
+
+  cout << " - Push to front all the characters from 'a' to 'y' in steps of 4, then reverse" << "\n";
+  for (char i = 'a'; i < 'z'; i += 4) {
+    my_list.push_front(i);
+  }
+  my_list.reverse();
+
+  forward_list<char>::iterator it = my_list.begin();
+  cout << " o Print the elements of my_list: ";
+  for (; it != my_list.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << "\n";
+
+  cout << " - Create a new list (my_other_list) initialised with (55, '+')" << "\n";
   forward_list<char> my_other_list(55, '+');
-  
-  // Get an iterator for the first element of my_other_list and print the list out.
+    
   it = my_other_list.begin();
   cout << " o Print the elements of my_other_list: ";
   for (; it != my_other_list.end(); ++it) {
@@ -135,38 +209,26 @@ int main(int argc, char* argv[]) {
   }
   cout << "\n";
 
-  // Copy the contents from my_list into my_other_list
+  cout << " - Copy the contets of my_list over my_other_list using the overloaded operator=" << "\n";
   my_other_list = my_list;
 
-  // Get an iterator for the first element of my_other_list and print the list out.
   it = my_other_list.begin();
-  cout << " o Print the elements of my_other_list (after copying my_list into it): ";
+  cout << " o Print the elements of my_other_list: ";
   for (; it != my_other_list.end(); ++it) {
     cout << *it << " ";
   }
   cout << "\n";
 
-  // Get an iterator for the first element of my_list and print the list out.
+  cout << " - Call a function that returns a new list (temporary by defininition) from a given one "
+    << "(my_other_list) and assign it to my_list using the overloaded operator= for moving data" << "\n";
+  my_list = addOne(my_other_list);
+
   it = my_list.begin();
   cout << " o Print the elements of my_list: ";
   for (; it != my_list.end(); ++it) {
     cout << *it << " ";
   }
   cout << "\n";
-  
-  // Move the temporary contents returned by the addOne function into my_list.
-  my_list = addOne(my_list);
-
-  // Get an iterator for the first element of my_list and print the list out.
-  it = my_list.begin();
-  cout << " o Print the elements of my_list: ";
-  for (; it != my_list.end(); ++it) {
-    cout << *it << " ";
-  }
-  cout << "\n";
-
-  system("pause");
-  return 0;
 }
 
 forward_list<char> addOne(const forward_list<char>& _forward_list) {
